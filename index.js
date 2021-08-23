0f9f8c64d73477e6781478e24dfb4a4c586a8141
Keira <KeiraJCoder@hotmail.com>
2021-08-23 16:22:32 +0100

    committed

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
+
+app.use(express.json()); // This ensures input is considered to be json
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello world");
 });
 
-app.get("/about", (req, res) => {
-    res.status(200).send("This is the about route");
+app.post("/:username/", (req, res) => {
+    res.status(201).json({"message": "repo created", "name": req.body.project});
+});
+
+app.get("/:username/:project", (req, res) => {
+    res.status(200).send(`You requested information about ${req.params.project} created by ${req.params.username}`);
 });
 
 app.listen(port, () => {
     console.log("App is online");
 });
-
-
-
