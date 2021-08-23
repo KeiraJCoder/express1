399d4917f3cb73422974b99c649362aa1802f157
Keira <KeiraJCoder@hotmail.com>
2021-08-23 11:51:14 +0100

    changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const express = require ("express");
+const app = express();
+const port = 80;
+
+
+//request and response
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
+})
+
+app.listen(port, () => {
+
+    console.log("App is online");
+});
+
+
+
+
