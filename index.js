2c9e33dba8f114b168eb2ad0e1d84b02ff1acd78
Keira <KeiraJCoder@hotmail.com>
2021-08-24 10:35:45 +0100

    updates

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 
 app.use(express.json()); // This ensures input is considered to be json
 
@@ -11,11 +12,15 @@ app.get("/", (req, res) => {
 });
 
 app.post("/:username/", (req, res) => {
-    res.status(201).json({"message": "repo created", "name": req.body.project});
+    res.status(201).json({"message": `You created the repo ${req.body.project}`, "data": req.body});
 });
 
 app.get("/:username/:project", (req, res) => {
-    res.status(200).send(`You requested information about ${req.params.project} created by ${req.params.username}`);
+    res.status(200).json({"message": `You views the project ${req.params.project}`});
+});
+
+app.post("/:username/:project", (req, res) => {
+    res.status(200).json({"message": `You updated the project: ${req.params.project}`, "date": req.body});
 });
 
 app.listen(port, () => {
